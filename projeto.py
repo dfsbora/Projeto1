ca8f8ddf55d82ee1e3524b8e957d90b944b5e1c9
dfsbora <dfsbora@gmail.com>
2018-04-20 21:00:11 -0300

    Criada função para plotar gráficos

--- projeto.py
+++ projeto.py
@@ -14,50 +14,64 @@ from math import *
 
 #%% Data initialization
 x_values = [0.1, 1.0, 10.0]
-
+table_up = []
+x_axis = range(0, 25)
 
 
 #%% Define function to compute j's
-
-table_up = [] #cria tabela vazia
-
-#table[row][column]     
-for x in x_values:
+def calculation():
+    #table_up = [] #cria tabela vazia #TODO mover para inicialização de dados, global
     
-    row_up = [] #cria lista vazia
-    #UP
-    j_up_ant = sin(x)/x #j0
-    row.append(j_up_ant) #adiciona item a lista
-    j_up_atual = sin(x)/(x**2) - cos(x)/x #j1  
-    row.append(j_up_atual) #adiciona item a lista  
-    for l in range(2, 25): #Loop de 2 a 24 (0 e 1 já definidos, totalizando 25)
-        j_up_prox = (2*l + 1)/x*j_up_atual - j_up_ant
-        row.append(j_up_prox) #adiciona item a lista
-        j_up_ant = j_up_atual 
-        j_up_atual = j_up_prox
-    
-    table_up.append(row) #adiciona lista a tabela
-     
-    #DOWN    
-    j_down_prox = j_up_atual
-    j_down_atual = j_up_ant
-    #guardar na tabela
-    for l in range(23, 0, -1):
-        j_down_ant = (2*l + 1)/x*j_down_atual - j_down_prox
+    #table[row][column]     
+    for x in x_values:
+        
+        row_up = [] #cria lista vazia
+        #UP
+        j_up_ant = sin(x)/x     #j0
+        row_up.append(j_up_ant)     #adiciona item a lista
+        j_up_atual = sin(x)/(x**2) - cos(x)/x   #j1  
+        row_up.append(j_up_atual)   #adiciona item a lista  
+        for l in range(2, 25):  #Loop de 2 a 24 (0 e 1 já definidos, totalizando 25)
+            j_up_prox = (2*l + 1)/x*j_up_atual - j_up_ant
+            row_up.append(j_up_prox)    #adiciona item a lista
+            j_up_ant = j_up_atual 
+            j_up_atual = j_up_prox
+        
+        table_up.append(row_up)     #adiciona lista a tabela
+         
+        #DOWN    
+        j_down_prox = j_up_atual
+        j_down_atual = j_up_ant
         #guardar na tabela
-        j_down_prox = j_down_atual 
-        j_down_atual = j_down_ant
-    
-        #print j_down_atual
+        for l in range(23, 0, -1):
+            j_down_ant = (2*l + 1)/x*j_down_atual - j_down_prox
+            #guardar na tabela
+            j_down_prox = j_down_atual 
+            j_down_atual = j_down_ant
+        j_down_zero = j_down_atual #TODO global ou então return, valor a ser usado na normalização
+        
+            #print j_down_atual
+
+#%% Define function to normalize down recursion
 
 
+
+
+#%% Define function to plot graphs
+
+def plot_():
+    for row in table_up:
+        plot(x_axis,row)
+        hold('on')
+
+     
 #%% Teste       
+calculation()      
+
+plot_()
+#%% Main 
+
+calculation()
     
-teste = [[00,01],[10,11],[20,21]]    
-print 'x: ', teste[2][0]    
-
-
-        
-        
         
     
